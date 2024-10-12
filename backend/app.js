d12b4fd1be0f865965704882c6a9b95a9ec52265
mohamuhsin <muhsinohoho@outlook.com>
2024-10-13 01:30:25 +0300

    few clean ups

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,24 @@
+const bodyParser = require("body-parser");
 const express = require("express");
-const cors = require("cors");
+
 const eventRoutes = require("./routes/events");
 const authRoutes = require("./routes/auth");
 
 const app = express();
 
-// Using express built-in JSON parser
-app.use(express.json());
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
 
-// Configure CORS
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Change to your frontend domain if different
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-// Set up routes
 app.use(authRoutes);
+
 app.use("/events", eventRoutes);
 
-// Error handling middleware
 app.use((error, req, res, next) => {
-  console.error(error); // Log error details
   const status = error.status || 500;
   const message = error.message || "Something went wrong.";
   res.status(status).json({ message: message });
